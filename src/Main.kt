3daba654fbffc4ff0b988ebd1e6506e641917162
William Alexander Matallana <wmatallana28@gmail.com>
2024-10-25 19:54:49 -0500

    Varibles Kotlin

---
+++ src/Main.kt
@@ -0,0 +1,89 @@
+fun main() {
+
+    // ELABORADO POR: ING. WILLIAM ALEXANDER MATALLANA PORRAS
+
+
+    // comentarios de 1 linea
+    /*
+
+    Comentarios multilinea
+
+     */
+
+    // variables -> var  variables que cambian
+    // constantes -> val variables que no cambian
+
+    // variables de tipo implicitas
+
+    println("VARIABLES DE TIPO IMPLICITAS")
+    var numero = 45  // Variable de tipo entero
+
+    var decimal = 12.5   // variable coma flotante
+
+    var texto = "Hola mundo"
+
+    val fecha = "2024-10-15"
+
+    val pi = 3.1416
+
+
+    println("El numero entero es: ${numero}")
+    println("El numero decimal es: ${decimal}")
+    println("El texto es: ${texto}")
+    println("La primer constante es: ${fecha}")
+    println("El numero PI: ${pi}")
+
+    // Variables explicitas
+
+    println("VARIABLES DE TIPO EXPLICITAS")
+    var numero1:Int = 56
+
+    var resultado:Double = 25.8
+
+    var caracter:Char = 'a'
+
+    println("La primer variable explicita es: ${numero1}")
+    println("La segunda vaiable explicita es: ${resultado}")
+    println("La tercer varibles explicita es: ${caracter}")
+
+    var n1 = 45
+    var n2:Int = 67
+
+    var suma = n1+n2
+    var resta = n1-n2
+    var multi = n1*n2
+
+    println("LA SUMA ENTRE ${n1} Y ${n2}  ES: ${suma}")
+    println("LA RESTA ENTRE ${n1} Y ${n2} ES: ${resta}")
+    println("LA MULTIPLICACION ENTRE ${n1} Y ${n2} ES: ${multi}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
